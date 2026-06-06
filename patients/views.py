396f3e92e2c715cb3680c72374a62ef11ee3c100
RamaTDev <resiliencia.ramirotevez@gmail.com>
2026-06-06 20:47:02 -0300

    modifi

--- patients/views.py
+++ patients/views.py
@@ -5,12 +5,3 @@ from rest_framework.decorators import api_view, permission_classes
 from authentication.permissions import EsTutor
 from .serializers import PacientesSerializer
 
-@api_view(['POST'])
-@permission_classes([EsTutor])
-def crear_paciente(request):
-
-    paciente = PacientesSerializer(data=request.data)
-
-    if paciente.is_valid():
-        paciente.save()
-
